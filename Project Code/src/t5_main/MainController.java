f7cfe665111f3f1c58a53e41cc24d440578ec0ac
jojojames <ja.nguyen@gmail.com>
2013-05-30 11:28:18 -0700

    asdf

---
+++ Project Code/src/t5_main/MainController.java
@@ -0,0 +1,10 @@
+package t5_main;
+import t5_view.LoginView;
+
+public class MainController {
+    public static void main(String[] args) {
+		//Applet applet = new HospitalApplet();
+		//Frame frame = new HospitalFrame(applet);
+        LoginView loginView = new LoginView();
+    }
+}
