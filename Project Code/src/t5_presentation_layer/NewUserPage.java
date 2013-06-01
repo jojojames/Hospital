38964d2955d27a63d50c1988fd4ce2b7e57a5832
Joel <joel.santiago1@gmail.com>
2013-06-01 11:32:25 -0700

    Made some GUI changes and Additions
    I created a more descriptive LogIn page, a NewUser page to replace the
    SignUp page, and a ScheduleSurgery page, i'll be working on some user
    pages as well, hopefully this commit works

---
+++ Project Code/src/t5_presentation_layer/NewUserPage.java
@@ -0,0 +1,34 @@
+package t5_presentation_layer;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joel
+ * Date: 6/1/13
+ * Time: 9:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewUserPage {
+    private JTextField firstNameTextField;
+    private JPanel panel1;
+    private JTextField lastNameTextField;
+    private JTextField dateOfBirthTextField;
+    private JComboBox comboBox1;
+    private JTextField XXXXXXXXXTextField;
+    private JTextField addressTextField;
+    private JTextField cityTextField;
+    private JComboBox comboBox2;
+    private JTextField homePhoneTextField;
+    private JTextField mobilePhoneTextField;
+    private JTextField emailAddressTextField;
+    private JTextField firstNameTextField1;
+    private JTextField lastNameTextField1;
+    private JTextField homePhoneTextField1;
+    private JTextField mobilePhoneTextField1;
+    private JTextField insuranceProviderTextField;
+    private JTextField insuranceAccountTextField;
+    private JButton clearButton;
+    private JButton registerButton;
+    private JButton cancelButton;
+}
---
+++ Project Code/src/t5_presentation_layer/ScheduleSurgery.java
@@ -0,0 +1,42 @@
+package t5_presentation_layer;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joel
+ * Date: 6/1/13
+ * Time: 10:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ScheduleSurgery {
+    private JPanel panel1;
+    private JTextField textField2;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextArea textArea1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+    private JTextField textField17;
+    private JTextField textField18;
+    private JTextField textField19;
+    private JCheckBox inpatientCheckBox;
+    private JCheckBox outpatientCheckBox;
+    private JButton cancelButton;
+    private JButton clearButton;
+    private JButton scheduleSurgeryButton;
+}
