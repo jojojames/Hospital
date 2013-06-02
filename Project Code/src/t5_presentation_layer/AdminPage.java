9e2fa23a72b6b70a26cd9485205805ec3e2bd6ec
Ronie Mendoza <mendoza.ronie@gmail.com>
2013-06-02 13:04:06 -0700

    added code from your AdminPage.Java

--- Project Code/src/t5_presentation_layer/AdminPage.java
+++ Project Code/src/t5_presentation_layer/AdminPage.java
@@ -2,6 +2,14 @@ package t5_presentation_layer;
 
 import javax.swing.*;
 
+/**
+ * Created with IntelliJ IDEA.
+ * User: james
+ * Date: 6/1/13
+ * Time: 7:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
 public class AdminPage {
     private JPanel panel1;
     private JTabbedPane tabbedPane1;
@@ -62,6 +70,21 @@ public class AdminPage {
     private JTable table3;
     private JTable table4;
 
+    private JPanel adminPagePanel;
+
+    JPanel contentPane;
+
+    public AdminPage(JPanel _contentPane) {
+        this.contentPane = _contentPane;
+    }
+
+    public JPanel getAdminPagePanel() {
+        return adminPagePanel;
+    }
+
+    public void setAdminPagePanel(JPanel adminPagePanel) {
+        this.adminPagePanel = adminPagePanel;
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
